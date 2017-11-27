957a950b01c7aa8e12fb61082b1a94454d0de1b2
cjzp <32529652+cjzp@users.noreply.github.com>
2017-11-27 17:07:02 -0500

    adición de standard en pom
    no funciona????

--- src/main/java/utils/MySqlDBConexion.java
+++ src/main/java/utils/MySqlDBConexion.java
@@ -8,7 +8,7 @@ public class MySqlDBConexion
 {
 	private final String URL = "jdbc:mysql://localhost:3306/bd_sistema?useSSL=false";
 	private final String USER = "root";
-	private final String PASS = "";
+	private final String PASS = "admin123";
 	
 	public MySqlDBConexion()
 	{}
