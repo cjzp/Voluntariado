2c1b69145cd4f64a5cee982844ed92195625f871
cjzp <32529652+cjzp@users.noreply.github.com>
2017-12-07 17:49:03 -0500

    Test pruebas 2
    parte 2

--- src/test/java/test/TestInstitucion.java
+++ src/test/java/test/TestInstitucion.java
@@ -15,11 +15,23 @@ public class TestInstitucion {
 	public void test() {
 		
 		InstitucionDTO insti = new InstitucionDTO();
+		insti.setCodigo(1005); //?
 		insti.setNombre("prueba");
+		insti.setCaracteristicas("caracteristicas de prueba");
+		insti.setDireccion("direccion de prueba");
+		insti.setComentarios("");
+		insti.setTelefono("telefono de prueba");
+		insti.setCalificacion(1);
+
 		
+		dao.registrarInstitucion(insti);
 		
+		InstitucionDTO recu = dao.buscarInstitucion(insti.getCodigo());
+		dao.eliminarInstitucion(insti.getCodigo());
 		
-		fail("Not yet implemented");
+			
+		assertTrue(insti.toString().equals(recu.toString()));
+		
 	}
 
 }
